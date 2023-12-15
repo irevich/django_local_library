76ce2e6e3cf8aeb7962751727daf8809abfc91aa
Igal Revich <irevich@itba.edu.ar>
2023-12-15 13:30:06 -0300

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['djangolocallibrary-production-1233.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://djangolocallibrary-production-1233.up.railway.app']
 
 # Application definition
 
